f137d22c48ebe97938af1ba8b3be9a076f92a6ba
Alfredo Fisher <alfredo.fisher44@gmail.com>
2022-07-26 18:09:22 -0300

    Initial remake commit
    Created 3 buttons to represent each of the player's choices. Added event listeners to each of those, and fixed the playRound function to respond accordingly. Commented out the game function for now, and next in line is adding a score tracking device.

--- index.js
+++ index.js
@@ -4,52 +4,69 @@ function computerPlay () {
     return roll;
 }   
 
-let computerSelection = computerPlay();
-let playerSelection = undefined;
+function playRound (a, b) {
+    alert(`The computer picked ${b}`);
+    if (a === b) {
+        alert("It's a tie!") ;
+    } else if (a == 'rock' && b =='paper') {
+        alert("You lose!");
+        computerScore++;
+    } else if (a == 'rock' && b == 'scissors') {
+        alert("You win!");
+        playerScore++;
+    } else if (a == 'paper' && b == 'rock') {
+        alert("You win!");
+        playerScore++;
+    } else if (a == 'paper' && b == 'scissors') {
+        alert("You lose!");
+        computerScore++;
+    } else if (a == 'scissors' && b == 'paper') {
+        alert("You win!");
+        playerScore++;
+    } else if (a == 'scissors' && b == 'rock') {
+        alert("You lose!");
+        computerScore++;
+    }
+    console.log(`the score is ${playerScore} to ${computerScore}`)
+}
+    
 let playerScore = 0;
 let computerScore = 0;
 
-function playRound (playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        alert("It's a tie!") ;
-    } else if (playerSelection == 'rock' && computerSelection =='paper') {
-        alert("You win!");
-        playerScore++;
-    } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
-        alert("You lose!");
-        computerScore++;
-    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
-        alert("You win!");
-        playerScore++;
-    } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-        alert("You lose!");
-        computerScore++;
-    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-        alert("You win!");
-        playerScore++;
-    } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-        alert("You lose!");
-        computerScore++;
-    } else {
-        alert("whaaaaa?");
-    }
-    }
-function game() {
-    for (let i = 0; i < 5; i++) {
-        computerSelection = computerPlay();
-        playerSelection = prompt('Choose your element!');
-        playRound(playerSelection.toLowerCase(), computerSelection);
-        alert(`the score is: ${playerScore} human ${computerScore} machine`);
+
+let scissors = document.getElementById('scissors');
+let paper = document.getElementById('paper');
+let rock = document.getElementById('rock');
+scissors.addEventListener('click', () => {
+    playRound('scissors', computerPlay())
+})
+rock.addEventListener('click', () => {
+    playRound('rock', computerPlay())
+})
+paper.addEventListener('click', () => {
+    playRound('paper', computerPlay())
+})
+
+
+
+
+
+//function game() {
+   // for (let i = 0; i < 5; i++) {
+   //     computerSelection = computerPlay();
+  //      playerSelection = prompt('Choose your element!');
+  //      playRound(playerSelection.toLowerCase(), computerSelection);
+  //      alert(`the score is: ${playerScore} human ${computerScore} machine`);
         
-     }
-}
+ //    }
+//}
 
-game();
+//game();
 
-if (computerScore > playerScore) {
-    alert("Fs in the chat, computer wins!");
-} else if (computerScore < playerScore) {
-    alert("You win the game! GGWW")
-} else {
-    alert("No way man! It's a bloody tie!")
-}
+//if (computerScore > playerScore) {
+//    alert("Fs in the chat, computer wins!");
+//} else if (computerScore < playerScore) {
+//    alert("You win the game! GGWW")
+//} else {
+ //   alert("No way man! It's a bloody tie!")
+//}
